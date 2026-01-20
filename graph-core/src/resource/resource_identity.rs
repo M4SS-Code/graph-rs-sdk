1ca16860c912bfab393f8566e9d3bbb8b95972ae
Paolo Barbolini <paolo.barbolini@m4ss.net>
2026-01-20 17:23:24 +0100

    Drop dependency on `remain`

--- graph-core/src/resource/resource_identity.rs
+++ graph-core/src/resource/resource_identity.rs
@@ -4,7 +4,6 @@ use std::fmt::Display;
 /// Comprises both top level and second level resources.
 /// These are not generated from OpenApi, except for top level resources,
 /// and mostly consist of Apis that the project currently has generated.
-#[remain::sorted]
 #[derive(
     AsRefStr,
     Copy,
